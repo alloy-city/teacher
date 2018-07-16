a5e2ecc61cf9450fc5504cd6aa98b5ff8f35fb42
bernardodesousa <bernardodesousa@gmail.com>
2018-07-16 10:41:00 -0300

    First step towards a better Teacher Mission Assessment tool.

--- src/mission/index.js
+++ src/mission/index.js
@@ -3,4 +3,11 @@ import { dismissOpenMissionAnswer } from './dismiss-open-mission-answer'
 
 getNotAssessedOpenMissions()
 
+document.addEventListener("keydown", e => {
+    if (e.shiftKey && e.code === "KeyM") {
+        let selection = window.getSelection()
+        console.log(selection.anchorNode.parentElement.parentElement.parentElement.id, selection.anchorOffset, selection.focusOffset)
+    }
+})
+
 export { dismissOpenMissionAnswer }
