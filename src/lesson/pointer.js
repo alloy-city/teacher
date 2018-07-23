6d4e82d3297b4db754f3a78d8c76018ebd9b9b40
Bernardo SOUSA <bernardodesousa@gmail.com>
2018-07-23 13:33:45 -0300

    Send pointer messages only when slide has been sent.

--- src/lesson/pointer.js
+++ src/lesson/pointer.js
@@ -4,30 +4,36 @@ function pointer(){
     let slideElements = document.getElementsByClassName("slide")
     for (let slide of slideElements) {
 
-        slide.addEventListener("pointerenter", () => {
-            console.log(slide.name, "start")
+        slide.addEventListener("pointerenter", e => {
+            if (e.currentTarget.parentElement.classList.contains("sent")){
+                console.log(slide.name, "start")
+            }
         })
 
         slide.addEventListener("pointermove", e => {
-            
-            let x = (e.pageX - e.currentTarget.offsetLeft) / e.currentTarget.width
-            let y = (e.pageY - e.currentTarget.offsetTop) / e.currentTarget.height
-
-            console.log(slide.name, "moving", x, y)
-
-            let instruction = {
-                to: Teacher.Course.selectedCourse.students,
-                from: Auth.userData._id,
-                type: "pointer coordinates",
-                slide: slide.name,
-                coordinates: [x, y]
+            if (e.currentTarget.parentElement.classList.contains("sent")){
+                let x = (e.pageX - e.currentTarget.offsetLeft) / e.currentTarget.width
+                let y = (e.pageY - e.currentTarget.offsetTop) / e.currentTarget.height
+    
+                console.log(slide.name, "moving", x, y)
+    
+                let instruction = {
+                    to: Teacher.Course.selectedCourse.students,
+                    from: Auth.userData._id,
+                    type: "pointer coordinates",
+                    slide: slide.name,
+                    coordinates: [x, y]
+                }
+    
+                eclassSocket.emit('instruction', instruction)
             }
-
-            eclassSocket.emit('instruction', instruction)
+            
         })
 
-        slide.addEventListener("pointerleave", () => {
-            console.log(slide.name, "end")
+        slide.addEventListener("pointerleave", e => {
+            if (e.currentTarget.parentElement.classList.contains("sent")){
+                console.log(slide.name, "end")
+            }
         })
     }
 }
